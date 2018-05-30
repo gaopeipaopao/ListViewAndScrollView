c265e867aa1a3fa321a0085b153a6e15b857acb9
gaopei <gaopeigpgp@126.com>
2018-05-30 18:13:26 +0800

    init

---
+++ app/src/main/java/com/example/gaope/listviewandscrollview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gaope.listviewandscrollview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * ListView和ScrollView的滑动冲突
+ * ExpandableListView的尝试
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
